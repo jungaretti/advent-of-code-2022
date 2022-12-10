3db51ffdc288a21f8b1c1157cacd68d6de4e3fc0
JP Ungaretti <git@jungaretti.com>
2022-12-10 06:13:15 +0000

    Create knot structure

--- day09/day9.py
+++ day09/day9.py
@@ -18,34 +18,36 @@ def parseMoves(lines):
                     moves.append((-1, 0))
     return moves
 
-def distance(new, old):
-    x = abs(new[0] - old[0])
-    y = abs(new[1] - old[1])
-    return max(x, y)
+class Knot:
+    def __init__(self, pos, tail = None) -> None:
+        self.pos = pos
+        self.tail = tail
+
+    def distance(self, toPos):
+        x = abs(toPos[0] - self.pos[0])
+        y = abs(toPos[1] - self.pos[1])
+        return max(x, y)
+
+    def drag(self, fromPos, toPos) -> tuple[int, int]:
+        old = self.pos
+        if self.distance(toPos) > 1:
+            self.pos = fromPos
+
+def play(moves, length = 1):
+    head = Knot((0, 0))
+    last = head
+    for _ in range(length):
+        last.tail = Knot(last.pos)
+        last = last.tail
+
+    solution = {}
+    for move in moves:
+        old = head.pos
+        head.pos = (head.pos[0] + move[0], head.pos[1] + move[1])
+        head.tail.drag(old, head.pos)
+        solution[head.tail.pos] = solution.get(head.tail.pos, 0) + 1
+    
+    return len(solution)
 
 moves = parseMoves(lines)
-
-# A -> B -> C -> D
-# move A, drag b
-
-class Tail:
-    def __init__(self, pos, next) -> None:
-        self.pos = pos
-        self.next = next
-
-# def drag(head, tail: Tail) -> tuple[int, int]:
-    
-def makeMoves(length):
-    solution = {}
-    head = (0, 0)
-    tail = Tail((0, 0), None)
-    for move in moves:
-        old = head
-        head = (head[0] + move[0], head[1] + move[1])
-        if distance(head, tail.pos) > 1:
-            tail.pos = old
-
-        solution[tail.pos] = solution.get(tail.pos, 0) + 1
-    return solution
-
-print(len(makeMoves(1)))
+print(play(moves))
