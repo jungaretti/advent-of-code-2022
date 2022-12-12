1c9b9d68861b2481b3b060f784d0ca25c7af1f0e
JP Ungaretti <git@jungaretti.com>
2022-12-12 02:43:49 +0000

    Solve day 9

--- day09/day9.py
+++ day09/day9.py
@@ -1,6 +1,6 @@
-lines: list[str] = []
-with open("day9.txt", "r") as file:
-    lines = file.read().splitlines()
+def readLines(filename):
+    with open(filename, "r") as file:
+        return file.read().splitlines()
 
 def parseMoves(lines):
     moves = []
@@ -19,35 +19,45 @@ def parseMoves(lines):
     return moves
 
 class Knot:
-    def __init__(self, pos, tail = None) -> None:
+    def __init__(self, pos) -> None:
         self.pos = pos
-        self.tail = tail
 
     def distance(self, toPos):
         x = abs(toPos[0] - self.pos[0])
         y = abs(toPos[1] - self.pos[1])
         return max(x, y)
 
-    def drag(self, fromPos, toPos) -> tuple[int, int]:
-        old = self.pos
+    def follow(self, toPos) -> tuple[int, int]:
         if self.distance(toPos) > 1:
-            self.pos = fromPos
+            cmp = lambda a, b: (a > b) - (a < b)
+            x = cmp(toPos[0], self.pos[0])
+            y = cmp(toPos[1], self.pos[1])
+            self.pos = (self.pos[0] + x, self.pos[1] + y)
+        return self.pos
 
-def play(moves, length = 1):
-    head = Knot((0, 0))
-    last = head
-    for _ in range(length):
-        last.tail = Knot(last.pos)
-        last = last.tail
+def play(moves, tailLength = 1):
+    knots = []
+    for _ in range(tailLength):
+        knots.append(Knot((0, 0)))
 
     solution = {}
+    headPos = (0, 0)
     for move in moves:
-        old = head.pos
-        head.pos = (head.pos[0] + move[0], head.pos[1] + move[1])
-        head.tail.drag(old, head.pos)
-        solution[head.tail.pos] = solution.get(head.tail.pos, 0) + 1
-    
+        headPos = (headPos[0] + move[0], headPos[1] + move[1])
+        toPos = headPos
+        for tailKnot in knots:
+            toPos = tailKnot.follow(toPos)
+        solution[toPos] = solution.get(toPos, 0) + 1
     return len(solution)
 
-moves = parseMoves(lines)
-print(play(moves))
+moves = parseMoves(readLines("day9example.txt"))
+print("P1 E1", play(moves))
+moves = parseMoves(readLines("day9.txt"))
+print("P1", play(moves))
+
+moves = parseMoves(readLines("day9example.txt"))
+print("P2 E1", play(moves, 10))
+moves = parseMoves(readLines("day9example2.txt"))
+print("P2 E2", play(moves, 9))
+moves = parseMoves(readLines("day9.txt"))
+print("P2", play(moves, 9))
